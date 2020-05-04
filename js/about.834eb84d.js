024c6055fe91dac9bf25d91773a3b3da19ee02a9
Yang Chen <chenyanghmilu@gmail.com>
2020-05-04 12:22:23 -0400

    deploy

---
+++ js/about.834eb84d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,s,a){"use strict";a.r(s);var e=function(){var t=this,s=t.$createElement,a=t._self._c||s;return a("div",{staticClass:"about"},[a("h1",[t._v("About")]),a("P",[t._v("This is the TodoList app about page, this is a vue class practice ")])],1)},i=[],u=a("2877"),n={},o=Object(u["a"])(n,e,i,!1,null,null,null);s["default"]=o.exports}}]);
+//# sourceMappingURL=about.834eb84d.js.map
